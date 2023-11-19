76dbeb298236ce28eef96db5522bcd6b4543e6f3
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-11-19 19:04:49 +0100

    start point

---
+++ src/main/java/com/myfood/MyFoodApplication.java
@@ -0,0 +1,13 @@
+package com.myfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfood/MyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
