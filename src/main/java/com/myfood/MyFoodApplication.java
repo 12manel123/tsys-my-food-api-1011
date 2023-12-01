9a437674d70e82f3b262f89fa6b8d418d1fd2948
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-01 04:46:46 +0100

    Test deploy

--- src/main/java/com/myfood/MyFoodApplication.java
+++ src/main/java/com/myfood/MyFoodApplication.java
@@ -2,13 +2,10 @@ package com.myfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories
-
 public class MyFoodApplication {
 
 	public static void main(String[] args) {
